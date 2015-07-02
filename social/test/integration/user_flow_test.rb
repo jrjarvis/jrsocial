de3ccbb6d4ba6c0aa758e42f3eb3a1e1f3bd1534
John Jarvis <jrjarvis@gmail.com>
2015-07-02 13:13:54 -0400

    Added initial integration test

---
+++ social/test/integration/user_flow_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UserFlowTest < ActionDispatch::IntegrationTest
+
+    test "user login, browse, logout" do 
+        user = users(:user1)
+
+        get "/login"
+
+        assert_response :success
+
+        post_via_redirect "/sessions", 
+            email: user.email,
+            password: "password"
+
+        assert_equal "/", path 
+
+        get_via_redirect "/logout"
+
+        assert_equal "/login", path 
+    end
+
+end

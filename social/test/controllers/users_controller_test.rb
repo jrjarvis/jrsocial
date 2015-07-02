680159fbc054129713f91aacc63b7540cd527767
John Jarvis <jrjarvis@gmail.com>
2015-07-02 13:03:33 -0400

    Added controller tests for user and post

--- social/test/controllers/users_controller_test.rb
+++ social/test/controllers/users_controller_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    def setup
+        #nothing
+    end
+
+    test "should get new with new user" do
+        get :new
+
+        user = assigns(:user)
+
+        assert user.new_record?
+        assert_response :success
+    end
+
+    test "should create user" do
+        params = {
+            user: {
+                email: "user@example.com",
+                password: "password",
+                password_confirmation: "password"
+            }
+        }
+
+        post :create, params
+
+        assert_redirected_to root_url
+    end
+
+    test "should not create user" do
+        params = {
+            user: {
+                email: "user@example.com",
+                password: "password",
+                password_confirmation: "password-bad"
+            }
+        }
+
+        post :create, params
+
+        assert_template :new
+    end
+
 end
